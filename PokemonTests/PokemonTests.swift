878c42af23c8688294475ff7a9fcb1dab56afe43
Gustavo Angulo <gustavo.p.angulo@gmail.com>
2025-06-09 21:18:40 -0300

    Initial Commit

---
+++ PokemonTests/PokemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTests.swift
+//  PokemonTests
+//
+//  Created by Gustavo Angulo on 09/06/25.
+//
+
+import Testing
+@testable import Pokemon
+
+struct PokemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
